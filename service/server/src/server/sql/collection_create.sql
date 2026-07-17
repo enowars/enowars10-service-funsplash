a2310a2aae3e1ddddd1b0bd49cf3e6af4d5f3155
Felix Dumbeck <felix@dumbeck.net>
2026-07-17 18:21:16 +0200

    correct collection create sql arg order

--- service/server/src/server/sql/collection_create.sql
+++ service/server/src/server/sql/collection_create.sql
@@ -1,3 +1,3 @@
-INSERT INTO collections (name, public_id,description, creator, private)
+INSERT INTO collections (public_id, name, description, creator, private)
 VALUES ($1, $2, nullif($3, ''), $4, $5)
 RETURNING *;
